0c89533713644f3c7c2454099fb5fc9abdea4bfa
sam690412-sketch <sam690412@gmail.com>
2026-05-29 15:41:33 +0800

    Update PaymentStatus.tsx

--- src/pages/PaymentStatus.tsx
+++ src/pages/PaymentStatus.tsx
@@ -1,6 +1,6 @@
 import { env } from "@/lib/env";
 import { useState, useEffect } from "react";
-import { useSearchParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
 import {
   Search,
   Loader2,
