5f9faa03160f2d3adb924dc9a77029d8ae0d844c
sam690412-sketch <sam690412@gmail.com>
2026-06-03 14:06:16 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import { Header } from "@/components/organism/Header";
 import { Footer } from "@/components/organism/Footer";
 import { CartDrawer } from "@/components/organism/CartDrawer";
